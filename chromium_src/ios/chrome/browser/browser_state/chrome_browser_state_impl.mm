6df678785b55e5d7e9f9f53a2eaf658be47d8559
mkarolin <max@brave.com>
2022-06-03 16:06:33 -0400

    [iOS] WIP: disable-enterprise-policy flag was removed.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/ba9c145917abfc83ba3180d9bdd7c27c9fcabb11
    
    commit ba9c145917abfc83ba3180d9bdd7c27c9fcabb11
    Author: Fabio Tirelo <ftirelo@chromium.org>
    Date:   Tue May 17 15:16:16 2022 +0000
    
        [Bling Enterprise] Remove --{en|dis}able-enterprise-policy
    
        Enterprise management support is fully launched for more than a year,
        and the switches are mostly used to enable/disable policies in tests.
    
        Removing those switches to ensure that tests run with the same
        configuration as production code.
    
        Bug: 1326188

---
+++ chromium_src/ios/chrome/browser/browser_state/chrome_browser_state_impl.mm
@@ -0,0 +1,22 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "ios/chrome/browser/browser_state/chrome_browser_state_impl.h"
+
+#include "ios/chrome/browser/policy/browser_policy_connector_ios.h"
+
+#define BrowserPolicyConnectorIOS if (false) { BrowserPolicyConnectorIOS
+#define BRAVE_CHROME_BROWSER_STATE_IMPL_CHROME_BROWSER_STATE_IMPL_CLOSE_IF }
+#define GetPolicyConnector GetPolicyConnector_ChromiumImpl
+
+#include "src/ios/chrome/browser/browser_state/chrome_browser_state_impl.mm"
+
+#undef GetPolicyConnector
+#undef BRAVE_CHROME_BROWSER_STATE_IMPL_CHROME_BROWSER_STATE_IMPL_CLOSE_IF
+#undef BrowserPolicyConnectorIOS
+
+BrowserStatePolicyConnector* ChromeBrowserStateImpl::GetPolicyConnector() {
+  return nullptr;
+}
