425da2716af72c9d797624422ff7285193908b62
Claudio DeSouza <cdesouza@igalia.com>
2022-06-07 19:11:14 +0100

    fixup! fixup! [iOS] WIP: disable-enterprise-policy flag was removed.

--- chromium_src/ios/chrome/browser/browser_state/chrome_browser_state_impl.mm
+++ chromium_src/ios/chrome/browser/browser_state/chrome_browser_state_impl.mm
@@ -5,7 +5,11 @@
 
 #include "ios/chrome/browser/browser_state/chrome_browser_state_impl.h"
 
+#include "ios/chrome/browser/application_context.h"
+#include "ios/chrome/browser/browser_state/off_the_record_chrome_browser_state_impl.h"
 #include "ios/chrome/browser/policy/browser_policy_connector_ios.h"
+#include "ios/chrome/browser/policy/browser_state_policy_connector.h"
+#include "ios/chrome/browser/policy/browser_state_policy_connector_factory.h"
 
 #define BrowserPolicyConnectorIOS \
   if (false) {                    \
