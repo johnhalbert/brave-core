8205267315b1483b16584162cd7c66739eae8079
Claudio DeSouza <cdesouza@igalia.com>
2022-06-08 14:37:28 +0100

    fixup! std::iterator deprecated on C++17

--- vendor/bat-native-ledger/src/bat/ledger/internal/publisher/prefix_iterator.h
+++ vendor/bat-native-ledger/src/bat/ledger/internal/publisher/prefix_iterator.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVELEDGER_PUBLISHER_PREFIX_ITERATOR_H_
-#define BRAVELEDGER_PUBLISHER_PREFIX_ITERATOR_H_
+#ifndef BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_PUBLISHER_PREFIX_ITERATOR_H_
+#define BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_PUBLISHER_PREFIX_ITERATOR_H_
 
 #include <iterator>
 
@@ -115,4 +115,4 @@ class PrefixIterator {
 }  // namespace publisher
 }  // namespace ledger
 
-#endif  // BRAVELEDGER_PUBLISHER_PREFIX_ITERATOR_H_
+#endif  // BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_PUBLISHER_PREFIX_ITERATOR_H_
